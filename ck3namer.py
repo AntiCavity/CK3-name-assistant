acc8a003a26d49d150cbe96ee487ab8ea4b80967
Nikola Pavlov <NPAVLOV@depaul.edu>
2023-03-21 10:46:27 -0500

    Created barebones GUI
    Used a customtkinter tutorial to make a very barebones gui, my next steps are to play around with customTkinter more and to eventually make all of it show up on the gui instead of in the terminal

--- ck3namer.py
+++ ck3namer.py
@@ -1,25 +1,61 @@
 import openai
 import creds
+import customtkinter
 
 openai.api_key = creds.API_KEY
-
 model = 'text-davinci-003'
 
-language = input('What language does your character speak? ')
+'''Here is the  backend'''
 
-location = input('What location do you wish to rename? ')
+def flavorize():
 
-prompt = 'How would someone who speaks %s pronounce and spell %s using the latin alphabet?' %(language, location)
+    language = input('What language does your character speak? ')
 
-#print(prompt)
+    location = input('What location do you wish to rename? ')
 
-response = openai.Completion.create(
-    prompt=prompt,
-    model=model,
-    max_tokens=1000,
-    temperature=0,
+    prompt = 'How would someone who speaks %s pronounce and spell %s using the latin alphabet?' %(language, location)
+
+
+    response = openai.Completion.create(
+        prompt=prompt,
+        model=model,
+        max_tokens=1000,
+        temperature=0.8,
     
-)
+    )
 
-for result in response.choices:
-    print(result.text)
+    for result in response.choices:
+        print(result.text)
+    
+
+'''Here is the frontend'''
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+root = customtkinter.CTk()
+root.geometry("500x350")    #here is the size of the interface
+
+
+    
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label=customtkinter.CTkLabel(master=frame, text="Login System or whateva", font = ("Roboto", 24))
+label.pack(pady=12,padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12,padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*") 
+entry2.pack(pady=12,padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Login", command=flavorize)
+button.pack(pady=12,padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
+checkbox.pack(pady=12,padx=10)
+
+root.mainloop()
+
+    
