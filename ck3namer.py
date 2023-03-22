e5545b661695f4540d227e695751b19257727eaa
Nikola Pavlov <NPAVLOV@depaul.edu>
2023-03-22 09:28:26 -0500

    Ck3 name assistant 1.0
    The first version has been complete, all the features work as intended, along with the GUI I made last push

--- ck3namer.py
+++ ck3namer.py
@@ -7,11 +7,12 @@ model = 'text-davinci-003'
 
 '''Here is the  backend'''
 
+
 def flavorize():
 
-    language = input('What language does your character speak? ')
+    language = langAns.get()
 
-    location = input('What location do you wish to rename? ')
+    location = provinceAns.get()
 
     prompt = 'How would someone who speaks %s pronounce and spell %s using the latin alphabet?' %(language, location)
 
@@ -25,8 +26,7 @@ def flavorize():
     )
 
     for result in response.choices:
-        print(result.text)
-    
+        retLabel.configure(text=result.text)
 
 '''Here is the frontend'''
 
@@ -35,26 +35,26 @@ customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
 root.geometry("500x350")    #here is the size of the interface
-
+root.title("CK3 Name Assistant")
 
     
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label=customtkinter.CTkLabel(master=frame, text="Login System or whateva", font = ("Roboto", 24))
+label=customtkinter.CTkLabel(master=frame, text="CK3 Name Assistant", font = ("Roboto", 24))
 label.pack(pady=12,padx=10)
 
-entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
-entry1.pack(pady=12,padx=10)
+provinceAns = customtkinter.CTkEntry(master=frame, placeholder_text="Province Name")
+provinceAns.pack(pady=12,padx=10)
 
-entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*") 
-entry2.pack(pady=12,padx=10)
+langAns = customtkinter.CTkEntry(master=frame, placeholder_text="Language") 
+langAns.pack(pady=12,padx=10)
 
-button = customtkinter.CTkButton(master=frame, text="Login", command=flavorize)
+button = customtkinter.CTkButton(master=frame, text="Generate Name", command=flavorize)
 button.pack(pady=12,padx=10)
 
-checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
-checkbox.pack(pady=12,padx=10)
+retLabel = customtkinter.CTkLabel(master=frame, text='', font = ("Roboto", 14))
+retLabel.pack()
 
 root.mainloop()
 
